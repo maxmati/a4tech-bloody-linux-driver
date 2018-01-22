8ac3427281e42ae98212bcf3229f2c4753108d8c
Timofey Titovets <nefelim4ag@gmail.com>
2018-01-22 05:36:25 +0300

    Added support: R8
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- Mouse.h
+++ Mouse.h
@@ -14,12 +14,13 @@ static const int BLOODY_V7_PID = 0xF613;
 static const int BLOODY_V8_PID = 0x11F5;
 static const int BLOODY_R7_PID = 0x1485;
 static const int BLOODY_R8_1_PID = 0x14ee;
+static const int BLOODY_R8_PID = 0x7c10;
 static const int BLOODY_R3_PID = 0x1a5a;
 static const int BLOODY_AL9_PID = 0xf633;
 static const int BLOODY_R70_PID = 0xf643;
 static const int BLOODY_A9_PID = 0x1003;
 
-static const int COMPATIBLE_PIDS[] = {BLOODY_V5_PID, BLOODY_V7_PID, BLOODY_V8_PID, BLOODY_R7_PID, BLOODY_R8_1_PID, BLOODY_R3_PID, BLOODY_AL9_PID, BLOODY_R70_PID, BLOODY_A9_PID};
+static const int COMPATIBLE_PIDS[] = {BLOODY_V5_PID, BLOODY_V7_PID, BLOODY_V8_PID, BLOODY_R7_PID, BLOODY_R8_1_PID, BLOODY_R8_PID, BLOODY_R3_PID, BLOODY_AL9_PID, BLOODY_R70_PID, BLOODY_A9_PID};
 static const size_t COMPATIBLE_PIDS_SIZE = sizeof(COMPATIBLE_PIDS)/sizeof(COMPATIBLE_PIDS[0]);
 
 static const int A4TECH_MAGIC = 0x07;
